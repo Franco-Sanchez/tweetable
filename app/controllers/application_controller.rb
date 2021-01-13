a417e89195d4ee80eb61cabeaf23e60386589afd
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-01-13 17:10:32 -0500

    Validations added, to be able to allow only correct information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameter, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameter
+    authorized = proc do |user|
+      user.permit(:email, :username,
+                  :name, :avatar,
+                  :password, :password_confirmation,
+                  :current_password)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up, &authorized)
+    devise_parameter_sanitizer.permit(:account_update, &authorized)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   has_many :tweets, dependent: :destroy
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/,
+                              message: 'It must be in a valid format: example@example.com' }
+  validates :name, presence: true
+  # I use allow_blank, to be able to edit a user without changing the password.
+  validates :password, length: { minimum: 6 }, allow_blank: true
 end
