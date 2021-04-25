fb0e54a35c174c49e7eaeb6f9af7fa8bf3f131cf
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-04-25 17:04:27 -0500

    Styles were added to the comments and the form to create a comment. Also, custom skyblue created

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        skyblue: {
+          50: '#f4fafe', 
+          100: '#e8f6fe', 
+          200: '#c7e8fc', 
+          300: '#a5d9fa', 
+          40: '#61bdf6', 
+          500: '#1da1f2', 
+          600: '#1a91da', 
+          700: '#1679b6', 
+          800: '#116191', 
+          900: '#0e4f77'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
